fba3eec275d8502292afc5d22d045bc2f22d4e57
JP Richardson <jprichardson@gmail.com>
2014-05-26 19:22:05 -0500

    remove binstring dep

--- test/bs58.test.js
+++ test/bs58.test.js
@@ -1,5 +1,4 @@
-var base58 = require('../lib/bs58')
-  , conv = require('binstring')
+var base58 = require('../')
 
 require('terst')
 
@@ -7,11 +6,11 @@ describe('+ encode(input)', function() {
   describe('> when input a byte array', function() {
     it('should convert to the base58 encoding', function() {
       var privateKeyAndChecksum = "801184CD2CDD640CA42CFC3A091C51D549B2F016D454B2774019C2B2D2E08529FD206EC97E";
-      var privateKeyWIF = base58.encode(conv(privateKeyAndChecksum, {in: 'hex', out: 'bytes'}));
+      var privateKeyWIF = base58.encode(new Buffer(privateKeyAndChecksum, 'hex'));
       EQ (privateKeyWIF, "5Hx15HFGyep2CfPxsJKe2fXJsCVn5DEiyoeGGF6JZjGbTRnqfiD");
 
       var unencodedAddress = "003c176e659bea0f29a3e9bf7880c112b1b31b4dc826268187";
-      var address = base58.encode(conv(unencodedAddress, {in: 'hex', out: 'bytes'}));
+      var address = base58.encode(new Buffer(unencodedAddress, 'hex'));
       EQ (address, "16UjcYNBG9GTK4uq2f7yYEbuifqCzoLMGS");
     })
   })
@@ -19,16 +18,8 @@ describe('+ encode(input)', function() {
   describe('> when input a buffer', function() {
     it('should convert to the base58 encoding', function() {
       var unencodedData = "003c176e659bea0f29a3e9bf7880c112b1b31b4dc826268187";
-      var b = new Buffer(conv(unencodedData, {in: 'hex', out: 'bytes'}));
+      var b = new Buffer(unencodedData, 'hex');
       var out = base58.encode(b);
-      EQ (out, "16UjcYNBG9GTK4uq2f7yYEbuifqCzoLMGS");
-    })
-  })
-
-  describe('> when input is a hex string', function() {
-    it('should convert to the base58 encoding', function() {
-      var unencodedData = "0x003c176e659bea0f29a3e9bf7880c112b1b31b4dc826268187";
-      var out = base58.encode(unencodedData);
       EQ (out, "16UjcYNBG9GTK4uq2f7yYEbuifqCzoLMGS");
     })
   })
