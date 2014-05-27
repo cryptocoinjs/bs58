9e579a0171318c6c0ef5fc2adc8fd975480a9b92
JP Richardson <jprichardson@gmail.com>
2014-05-27 17:02:39 -0500

    update fixtures, tests

--- test/bs58.test.js
+++ test/bs58.test.js
@@ -1,35 +1,30 @@
 var base58 = require('../')
+var fixtures = require('./fixtures')
 
 require('terst')
 
 describe('+ encode(input)', function() {
-  describe('> when input a byte array', function() {
-    it('should convert to the base58 encoding', function() {
-      var privateKeyAndChecksum = "801184CD2CDD640CA42CFC3A091C51D549B2F016D454B2774019C2B2D2E08529FD206EC97E";
-      var privateKeyWIF = base58.encode(new Buffer(privateKeyAndChecksum, 'hex'));
-      EQ (privateKeyWIF, "5Hx15HFGyep2CfPxsJKe2fXJsCVn5DEiyoeGGF6JZjGbTRnqfiD");
-
-      var unencodedAddress = "003c176e659bea0f29a3e9bf7880c112b1b31b4dc826268187";
-      var address = base58.encode(new Buffer(unencodedAddress, 'hex'));
-      EQ (address, "16UjcYNBG9GTK4uq2f7yYEbuifqCzoLMGS");
-    })
-  })
-
-  describe('> when input a buffer', function() {
-    it('should convert to the base58 encoding', function() {
-      var unencodedData = "003c176e659bea0f29a3e9bf7880c112b1b31b4dc826268187";
-      var b = new Buffer(unencodedData, 'hex');
-      var out = base58.encode(b);
-      EQ (out, "16UjcYNBG9GTK4uq2f7yYEbuifqCzoLMGS");
+  it('should properly encode hex values', function() {
+    fixtures.valid.forEach(function(f) {
+      var out = base58.encode(new Buffer(f.hex, 'hex'))
+      EQ (out, f.string)
     })
   })
 })
 
 describe('+ decode(input)', function() {
-  describe('> when a byte array is input', function() {
-    it('should convert to a a byte array', function() {
-      var address = "16UjcYNBG9GTK4uq2f7yYEbuifqCzoLMGS";
-      EQ (base58.decode(address).toString('hex'), "003c176e659bea0f29a3e9bf7880c112b1b31b4dc826268187");
+  it('should properly decode base58 encoded values', function() {
+    fixtures.valid.forEach(function(f) {
+      var out = base58.decode(f.string)
+      EQ (out.toString('hex'), f.hex)
+    })
+  })
+
+  fixtures.invalid.forEach(function(f) {
+    it('throws on ' + f.description, function() {
+      THROWS (function() {
+        base58.decode(f.string)
+      })
     })
   })
 })
