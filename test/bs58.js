950cda4a274070a256d9653d9fb49509347174ea
Daniel Cousens <github@dcousens.com>
2014-06-26 18:14:22 +1000

    tests: switch to for(it( style

---
+++ test/bs58.js
@@ -0,0 +1,34 @@
+var assert = require('assert')
+var base58 = require('../lib/bs58')
+
+var fixtures = require('./fixtures.json')
+
+describe('base58', function() {
+  describe('encode', function() {
+    fixtures.valid.forEach(function(f) {
+      it('can encode ' + f.hex, function() {
+        var actual = base58.encode(new Buffer(f.hex, 'hex'))
+
+        assert.strictEqual(actual, f.string)
+      })
+    })
+  })
+
+  describe('decode', function() {
+    fixtures.valid.forEach(function(f) {
+      it('can decode ' + f.string, function() {
+        var actual = base58.decode(f.string).toString('hex')
+
+        assert.strictEqual(actual, f.hex)
+      })
+    })
+
+    fixtures.invalid.forEach(function(f) {
+      it('throws on ' + f.description, function() {
+        assert.throws(function() {
+          base58.decode(f.string)
+        }, /invalid base58 string/)
+      })
+    })
+  })
+})
