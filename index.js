df80db445f04aa8ee6f0b449a2fb313faa36cc5d
Daniel Cousens <github@dcousens.com>
2015-09-01 02:07:08 +1000

    index: avoid unnecessary base-x unpack

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 var basex = require('base-x')
 var ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-var base58 = basex(ALPHABET)
 
-module.exports = {
-  encode: base58.encode,
-  decode: base58.decode
-}
+module.exports = basex(ALPHABET)
