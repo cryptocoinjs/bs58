6111202d52d417452852d60763ac84a97814079e
Jakob Krigovsky <jakob@krigovsky.com>
2019-08-18 13:11:06 +0200

    Use const instead of var

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var basex = require('base-x')
-var ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+const basex = require('base-x')
+const ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
 module.exports = basex(ALPHABET)
--- test/index.js
+++ test/index.js
@@ -1,13 +1,13 @@
-var test = require('tape')
-var base58 = require('../')
+const test = require('tape')
+const base58 = require('../')
 
-var fixtures = require('./fixtures.json')
+const fixtures = require('./fixtures.json')
 
 test('base58', function (t) {
   t.test('encode', function (t) {
     fixtures.valid.forEach(function (f) {
       t.test('can encode ' + f.hex, function (t) {
-        var actual = base58.encode(Buffer.from(f.hex, 'hex'))
+        const actual = base58.encode(Buffer.from(f.hex, 'hex'))
         t.equal(actual, f.string)
         t.end()
       })
@@ -19,7 +19,7 @@ test('base58', function (t) {
   t.test('decode', function (t) {
     fixtures.valid.forEach(function (f) {
       t.test('can decode ' + f.string, function (t) {
-        var actual = base58.decode(f.string).toString('hex')
+        const actual = base58.decode(f.string).toString('hex')
         t.same(actual, f.hex)
         t.end()
       })
